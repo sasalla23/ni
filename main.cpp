553c70323ddbaad7022547c683a34ef6d064f727
sasalla23 <johannesbockhorst2@gmail.com>
2023-09-22 21:37:28 +0200

    Add more operators, use precedence climbing

--- main.cpp
+++ main.cpp
@@ -47,10 +47,28 @@ std::ostream& operator<<(std::ostream& output_stream, const Location& location) 
 
 #define TOKEN_TYPE_LIST \
     TOKEN_TYPE_ENTRY(INT_LITERAL) \
+    \
     TOKEN_TYPE_ENTRY(PLUS) \
     TOKEN_TYPE_ENTRY(MINUS) \
     TOKEN_TYPE_ENTRY(STAR) \
     TOKEN_TYPE_ENTRY(SLASH) \
+    TOKEN_TYPE_ENTRY(BANG) \
+    TOKEN_TYPE_ENTRY(TILDE) \
+    TOKEN_TYPE_ENTRY(PERCENT) \
+    TOKEN_TYPE_ENTRY(LESS_LESS) \
+    TOKEN_TYPE_ENTRY(GREATER_GREATER) \
+    TOKEN_TYPE_ENTRY(LESS) \
+    TOKEN_TYPE_ENTRY(LESS_EQUAL) \
+    TOKEN_TYPE_ENTRY(GREATER) \
+    TOKEN_TYPE_ENTRY(GREATER_EQUAL) \
+    TOKEN_TYPE_ENTRY(EQUAL_EQUAL) \
+    TOKEN_TYPE_ENTRY(BANG_EQUAL) \
+    TOKEN_TYPE_ENTRY(AND) \
+    TOKEN_TYPE_ENTRY(HAT) \
+    TOKEN_TYPE_ENTRY(PIPE) \
+    TOKEN_TYPE_ENTRY(AND_AND) \
+    TOKEN_TYPE_ENTRY(PIPE_PIPE) \
+    \
     TOKEN_TYPE_ENTRY(OPEN_PARENTHESIS) \
     TOKEN_TYPE_ENTRY(CLOSE_PARENTHESIS) \
     TOKEN_TYPE_ENTRY(END_OF_FILE) \
@@ -179,6 +197,97 @@ public:
                 return slash_token;
             }
             
+            case '!': {
+                Location token_location = this->current_location;
+                this->advance_char();
+                if (this->curr_char() == '=') {
+                    this->advance_char();
+                    return Token(TokenType::BANG_EQUAL, "!=", this->current_location);
+                } else {
+                    return Token(TokenType::BANG, "!", this->current_location);
+                }
+            }
+            
+            case '~': {
+                Token tilde_token = Token(TokenType::TILDE, "~", this->current_location);
+                this->advance_char();
+                return tilde_token;
+            }
+            
+            case '%': {
+                Token percent_token = Token(TokenType::PERCENT, "%", this->current_location);
+                this->advance_char();
+                return percent_token;
+            }
+            
+            case '<': {
+                Location token_location = this->current_location;
+                this->advance_char();
+                if (this->curr_char() == '<') {
+                    this->advance_char();
+                    return Token(TokenType::LESS_LESS, "<<", this->current_location);
+                } else if (this->curr_char() == '=') {
+                    this->advance_char();
+                    return Token(TokenType::LESS_EQUAL, "<=", this->current_location);
+                } else {
+                    return Token(TokenType::LESS, "<", this->current_location);
+                }
+            }
+            
+            case '>': {
+                Location token_location = this->current_location;
+                this->advance_char();
+                if (this->curr_char() == '>') {
+                    this->advance_char();
+                    return Token(TokenType::GREATER_GREATER, ">>", this->current_location);
+                } else if (this->curr_char() == '=') {
+                    this->advance_char();
+                    return Token(TokenType::GREATER_EQUAL, ">=", this->current_location);
+                } else {
+                    return Token(TokenType::GREATER, ">", this->current_location);
+                }
+            }
+            
+            case '=': {
+                Location token_location = this->current_location;
+                this->advance_char();
+                if (this->curr_char() == '=') {
+                    this->advance_char();
+                    return Token(TokenType::EQUAL_EQUAL, "==", this->current_location);
+                } else {
+                    std::cerr << token_location << ": LEX_ERROR: Expected '=' after '=' character" << std::endl;
+                    std::exit(1);
+                }
+            }
+            
+            case '&': {
+                Location token_location = this->current_location;
+                this->advance_char();
+                if (this->curr_char() == '&') {
+                    this->advance_char();
+                    return Token(TokenType::AND_AND, "&&", this->current_location);
+                } else {
+                    return Token(TokenType::AND, "&", this->current_location);
+                }
+            }
+            
+            case '|': {
+                Location token_location = this->current_location;
+                this->advance_char();
+                if (this->curr_char() == '|') {
+                    this->advance_char();
+                    return Token(TokenType::PIPE_PIPE, "||", this->current_location);
+                } else {
+                    return Token(TokenType::PIPE, "|", this->current_location);
+                }
+            }
+            
+            case '^': {
+                Location token_location = this->current_location;
+                this->advance_char();
+                return Token(TokenType::HAT, "^", this->current_location);
+            }
+            
             case '(': {
                 Token open_parenthesis_token = Token(TokenType::OPEN_PARENTHESIS, "(", this->current_location);
                 this->advance_char();
@@ -198,7 +307,7 @@ public:
             default: {
                 if (std::isdigit(this->curr_char())) {
                     size_t start_pointer = this->source_pointer;
-                    Location start_location(this->current_location);
+                    Location start_location = this->current_location;
                     
                     while (std::isdigit(this->curr_char())) {
                         this->advance_char();
@@ -258,13 +367,12 @@ private:
     std::unique_ptr<Expression> left, right;
     Token operator_token;
 public:
-
-    BinaryExpression(std::unique_ptr<Expression> left, std::unique_ptr<Expression> right, const Token& operator_token)
-        : Expression(left->get_location()), operator_token(operator_token)
-    {
-        this->left = std::move(left);
-        this->right = std::move(right);
-    }
+    BinaryExpression(std::unique_ptr<Expression> left, std::unique_ptr<Expression> right, const Token& operator_token) :
+        Expression(left->get_location()),
+        left(std::move(left)),
+        right(std::move(right)),
+        operator_token(operator_token)
+    {}
     
     virtual void append_to_output_stream(std::ostream& output_stream, size_t layer = 0) const override {
         for (size_t i = 0; i < layer; i++) {
@@ -296,15 +404,31 @@ public:
     ~LiteralExpression() {}
 };
 
+class UnaryExpression : public Expression {
+private:
+    Token operator_token;
+    std::unique_ptr<Expression> operand;
+public:
+    UnaryExpression(const Token& operator_token, std::unique_ptr<Expression> operand)
+        : Expression(operator_token.get_location()), operator_token(operator_token), operand(std::move(operand))
+    {}
+
+    virtual void append_to_output_stream(std::ostream& output_stream, size_t layer = 0) const override {
+        for (size_t i = 0; i < layer; i++) {
+            output_stream << "\t";
+        }
+        output_stream << "UnaryExpression(" << this->operator_token.get_text() << ")" << std::endl;
+        this->operand->append_to_output_stream(output_stream, layer+1);
+    }
+
+    ~UnaryExpression() {}
+};
+
 class Parser {
 private:
     std::vector<Token> tokens;
     size_t token_pointer;
-public:
-    Parser(std::vector<Token> tokens)
-        : tokens(tokens), token_pointer(0)
-    {}
-
+    
     Token get_current_token() const {
         assert(tokens.size() > 0);
         if (this->token_pointer >= this->tokens.size()) {
@@ -328,49 +452,96 @@ public:
         return current_token;
     }
 
-    std::unique_ptr<Expression> parse_expression() {
-        return parse_additive_expression();
-    }
-
-    std::unique_ptr<Expression> parse_additive_expression() {
-        auto left = this->parse_multiplicative_expression();
-        Token next_operator = get_current_token();
-        while (next_operator.get_type() == TokenType::PLUS || next_operator.get_type() == TokenType::MINUS) {
-            (void)this->consume_token();
-            auto right = this->parse_multiplicative_expression();
-            left = std::make_unique<BinaryExpression>(std::move(left), std::move(right), next_operator);
-            next_operator = this->get_current_token();
+    // Refer to https://en.cppreference.com/w/c/language/operator_precedence
+    int get_binary_precedence(TokenType token_type) {
+        switch (token_type) {
+            case TokenType::STAR:
+            case TokenType::SLASH:
+            case TokenType::PERCENT:
+                return 3;
+            case TokenType::PLUS:
+            case TokenType::MINUS:
+                return 4;
+            case TokenType::LESS_LESS:
+            case TokenType::GREATER_GREATER:
+                return 5;
+            case TokenType::LESS:
+            case TokenType::GREATER:
+            case TokenType::LESS_EQUAL:
+            case TokenType::GREATER_EQUAL:
+                return 6;
+            case TokenType::EQUAL_EQUAL:
+            case TokenType::BANG_EQUAL:
+                return 7;
+            case TokenType::AND:
+                return 8;
+            case TokenType::HAT:
+                return 9;
+            case TokenType::PIPE:
+                return 10;
+            case TokenType::AND_AND:
+                return 11;
+            case TokenType::PIPE_PIPE:
+                return 12;
+            default:
+                return 0; // Not an operator
         }
-        return left;
+    }
+public:
+    Parser(std::vector<Token> tokens)
+        : tokens(tokens), token_pointer(0)
+    {}
+
+
+    std::unique_ptr<Expression> parse_expression() {
+        return this->parse_binary_expression();
     }
 
-    std::unique_ptr<Expression> parse_multiplicative_expression() {
+private:
+    std::unique_ptr<Expression> parse_binary_expression(int parent_precedence=-1) {
         auto left = this->parse_primary_expression();
         Token next_operator = this->get_current_token();
-        while (next_operator.get_type() == TokenType::STAR || next_operator.get_type() == TokenType::SLASH) {
+        int operator_precedence = this->get_binary_precedence(next_operator.get_type());
+        while (
+                (parent_precedence == -1 && operator_precedence != 0) ||
+                (0 < operator_precedence && operator_precedence < parent_precedence)
+        ) {
             (void)this->consume_token();
-            auto right = this->parse_primary_expression();
+            auto right = this->parse_binary_expression(operator_precedence);
             left = std::make_unique<BinaryExpression>(std::move(left), std::move(right), next_operator);
             next_operator = this->get_current_token();
+            operator_precedence = this->get_binary_precedence(next_operator.get_type());
         }
         return left;
     }
 
     std::unique_ptr<Expression> parse_primary_expression() {
         Token current_token = this->get_current_token();
-        if (current_token.get_type() == TokenType::INT_LITERAL) {
-            return std::make_unique<LiteralExpression>(this->consume_token());
-        } else if (current_token.get_type() == TokenType::OPEN_PARENTHESIS) {
-            (void)this->consume_token();
-            auto inner_expression = parse_expression();
-            this->expect_token(TokenType::CLOSE_PARENTHESIS);
-            return inner_expression;
-        } else {
-            std::cerr << current_token.get_location() << ": PARSE_ERROR: Unexpected token of type <" << current_token.get_type() << "> at the beginning of a primary expression." << std::endl;
-            std::exit(1);
+        switch (current_token.get_type()) {
+            case TokenType::INT_LITERAL:
+                return std::make_unique<LiteralExpression>(this->consume_token());
+
+            case TokenType::OPEN_PARENTHESIS: {
+                (void)this->consume_token();
+                auto inner_expression = this->parse_expression();
+                this->expect_token(TokenType::CLOSE_PARENTHESIS);
+                return inner_expression;
+            }
+
+            case TokenType::BANG:
+            case TokenType::MINUS:
+            case TokenType::PLUS: {
+                Token operator_token = this->consume_token();
+                auto operand = this->parse_primary_expression();
+                return std::make_unique<UnaryExpression>(operator_token, std::move(operand));
+            }
+            default:
+                std::cerr << current_token.get_location() << ": PARSE_ERROR: Unexpected token of type <" << current_token.get_type() << "> at the beginning of a primary expression." << std::endl;
+                std::exit(1);
         }
     }
 
+public:
     ~Parser() {}
 };
 
